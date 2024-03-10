48dfe82a3a54b8b4aae18f3f0750bed8bc1ed0c7
laurenp94 <laurenp94@vt.edu>
2024-03-09 22:47:48 -0500

    New Announced Semiconductor Facilitites List

--- MergeDatasets/NewFacilities.py
+++ MergeDatasets/NewFacilities.py
@@ -9,5 +9,11 @@ semiconductor_df = pd.read_csv(file_path)
 # Delete rows where "Existing_Announced" column does not contain "Announced New Project"
 semiconductor_df = semiconductor_df[semiconductor_df['Existing_Announced'].str.contains('Announced New Project')]
 
+# Drop the "Source" column
+semiconductor_df = semiconductor_df.drop(columns=['Source'])
+
+# Save the updated dataframe to the CSV file
+semiconductor_df.to_csv("MergeDatasets/SemiconductorFacilities_updated.csv", index=False)
+
 # Print the dataframe
 print(semiconductor_df)
